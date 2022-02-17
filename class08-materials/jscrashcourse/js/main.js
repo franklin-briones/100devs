8b81d95496b63b707c650978da00f03958a007e2
Franklin Briones <franklindbriones@gmail.com>
2022-02-17 17:52:44 -0600

    adding class work

---
+++ class08-materials/jscrashcourse/js/main.js
@@ -0,0 +1,34 @@
+// let - can reassign, const-cannot reasssign
+
+//Data types
+//string, numbers, boolean, null, undefined, symbols
+
+// //String
+// const name='John';
+// //number
+// const age=30;
+// const rating=4.5;
+// //boolean
+// const isCool=true;
+// //mull
+// const x=null;
+// //undefined
+// const y=undefined;
+// let z;
+
+// //test the type
+// console.log(typeof x);
+
+//Strings ----------------
+// const myName='John';
+// const age=30;
+
+// //Concatenation
+// //console.log('My name is ' + myName + ' and I am ' + age)
+// //Template string
+// console.log(`My name is ${myName} and I am ${age}`)
+// const hello=`My name is ${myName} and I am ${age}`;
+// console.log(hello);
+
+// const s='technology, computers, it, code';
+// console.log(s.split(', '));
